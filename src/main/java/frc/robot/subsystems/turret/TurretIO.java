18f9515aca9da0f26ba57f07a1881cfe48d87328
Maro1810 <145640219+Maro1810@users.noreply.github.com>
2026-07-11 11:57:20 -0700

    started turret code

--- src/main/java/frc/robot/subsystems/turret/TurretIO.java
+++ src/main/java/frc/robot/subsystems/turret/TurretIO.java
@@ -1,5 +1,17 @@
 package frc.robot.subsystems.turret;
 
 public interface TurretIO {
-    
+
+    public class TurretIOInputs {
+        public double angle;
+        public double appliedVolts;
+        public double statorCurrent;
+        public double supplyCurrent;
+    }
+
+    public void setAngle(double angle);
+    public void resetEncoder();
+    public void updateInputs(TurretIOInputs inputs);
+    public void stop();
+
 }
--- src/main/java/frc/robot/subsystems/turret/TurretIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/turret/TurretIOTalonFX.java
@@ -1,5 +1,117 @@
 package frc.robot.subsystems.turret;
 
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.HardwareLimitSwitchConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Voltage;
+import static frc.robot.subsystems.turret.Turret.TurretConstants.*;
+
 public class TurretIOTalonFX implements TurretIO{
+    //need to specify upper or lower CAN bus
+    protected final TalonFX m_turretMotor = new TalonFX(kCANID); 
+    protected final MotionMagicVoltage positionRequest = new MotionMagicVoltage(0).withEnableFOC(true);
+    
+    protected StatusSignal<Angle> angleSignal;
+    protected StatusSignal<Voltage> voltSignal;
+    protected StatusSignal<Current> statorCurrentSignal;
+    protected StatusSignal<Current> supplyCurrentSignal;
+
+    public TurretIOTalonFX() {
+        configMotor();
+
+        angleSignal = m_turretMotor.getPosition();
+        voltSignal = m_turretMotor.getMotorVoltage();
+
+        statorCurrentSignal = m_turretMotor.getStatorCurrent();
+        supplyCurrentSignal = m_turretMotor.getSupplyCurrent();
+    }
+
+    public void configMotor() {
+        TalonFXConfiguration config = new TalonFXConfiguration();
+
+        config.MotorOutput = 
+            new MotorOutputConfigs()
+                .withNeutralMode(NeutralModeValue.Coast)
+                .withInverted(InvertedValue.CounterClockwise_Positive);
+        
+        config.CurrentLimits = 
+            new CurrentLimitsConfigs()
+                .withStatorCurrentLimit(kStatorCurrentLimitAmps)
+                .withStatorCurrentLimitEnable(true)
+                .withSupplyCurrentLimit(kSupplyCurrentLimitAmps)
+                .withSupplyCurrentLimitEnable(true);
+        
+        config.Feedback = 
+            new FeedbackConfigs().withSensorToMechanismRatio(kReduction);
+
+        config.Slot0 = 
+            new Slot0Configs()
+                .withKP(kP)
+                .withKV(kV)
+                .withKA(kA);
+        
+        config.SoftwareLimitSwitch = 
+            new SoftwareLimitSwitchConfigs()
+                .withForwardSoftLimitEnable(true)
+                .withForwardSoftLimitThreshold(angleToRotations(0)) //fix
+                .withReverseSoftLimitEnable(true)
+                .withReverseSoftLimitThreshold(0);
+
+        config.HardwareLimitSwitch =
+            new HardwareLimitSwitchConfigs()
+                .withForwardLimitEnable(true)
+                .withReverseLimitEnable(true);
+
+        //TODO replace this with CtreUtil reportIfNotOk
+        m_turretMotor.getConfigurator().apply(config);
+    }
+
+    @Override
+    public void resetEncoder() {
+        m_turretMotor.setPosition(0);
+    }
+
+    @Override
+    public void updateInputs(TurretIOInputs inputs) {
+        BaseStatusSignal.refreshAll(angleSignal, voltSignal, statorCurrentSignal, supplyCurrentSignal);
+
+        // inputs.angle = rotationsToAngle(angleSignal.getValueAsDouble());
+        inputs.appliedVolts = voltSignal.getValueAsDouble();
+        inputs.statorCurrent = statorCurrentSignal.getValueAsDouble();
+        inputs.supplyCurrent = supplyCurrentSignal.getValueAsDouble();
+    }
+
+    @Override
+    public void setAngle(double angle) {
+
+    }
+    
+    //fix
+    protected double angleToRotations(double angle) {
+        return 0.0;
+    }
+
+    //fix
+    protected double rotationsToAngle(double rotations) {
+        return 0.0;
+    }
+
+    @Override
+    public void stop() {
+        m_turretMotor.stopMotor();
+    }
     
 }
