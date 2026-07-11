b39a4089deffb61c29b82179cc421146a2324216
Maro1810 <145640219+Maro1810@users.noreply.github.com>
2026-07-11 11:42:40 -0700

    added base TurretConstants

--- src/main/java/frc/robot/subsystems/turret/Turret.java
+++ src/main/java/frc/robot/subsystems/turret/Turret.java
@@ -1,5 +1,28 @@
 package frc.robot.subsystems.turret;
 
 public class Turret {
-    
+
+    static class TurretConstants {
+        public static int kCANID = 0; //have to figure this out
+
+        //Tune PID/FF constants
+        public static final double kP = 0;
+        public static final double kI = 0;
+        public static final double kD = 0;
+        public static final double kS = 0;
+        public static final double kV = 0;
+        public static final double kA = 0;
+
+        public static final double kSimP = 0;
+        public static final double kSimI = 0;
+        public static final double kSimD = 0;
+        public static final double kSimS = 0;
+        public static final double kSimV = 0;
+        public static final double kSimA = 0;
+
+        public static final double kStatorCurrentLimitAmps = 0;
+        public static final double kSupplyCurrentLimitAmps = 0;
+
+        public static final double kReduction = 32.5;
+    }
 }
