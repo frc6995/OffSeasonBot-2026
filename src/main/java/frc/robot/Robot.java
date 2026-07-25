b6ba7275a614e1b2e9e45edc8bcab13cfe9bdcb8
theYoshiDM <theyoshidm@gmail.com>
2026-07-24 19:28:03 -0700

    Deleted notifications

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -24,12 +24,6 @@ public class Robot extends TimedRobot {
         .withJoystickReplay();
     
 
-    Elastic.Notification notification = new Elastic.Notification(
-        //Level of notificaction level
-        Elastic.NotificationLevel.ERROR,
-        "Need Notificaction?", 
-        "This is an example error notification.");
-
     public Robot() {
         m_robotContainer = new RobotContainer();
     }
@@ -59,8 +53,6 @@ public class Robot extends TimedRobot {
         }
         //Tab switching so when we start, tab switches to "Autonomous".
         Elastic.selectTab("Autonomous");
-        //Send a notification
-        //Elastic.sendNotification(notification);
     }
 
     @Override
