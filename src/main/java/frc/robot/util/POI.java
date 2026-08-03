b82b00dcb9be9fe9cce53c27f90112f181786963
Jeremy <maplecake199@gmail.com>
2026-08-03 08:56:50 -0700

    Added passing logic and tuned to field

--- src/main/java/frc/robot/util/POI.java
+++ src/main/java/frc/robot/util/POI.java
@@ -3,7 +3,9 @@ package frc.robot.util;
 import java.util.function.Supplier;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rectangle2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import frc.robot.generated.ChoreoVars;
 import choreo.Choreo;
 import choreo.util.ChoreoAllianceFlipUtil;
@@ -20,6 +22,15 @@ public class POI {
     public static final Supplier<Pose2d> M_2 = flipped(ChoreoVars.Poses.M_2);
     public static final Supplier<Pose2d> M_3 = flipped(ChoreoVars.Poses.M_3);
     public static final Supplier<Pose2d> HUB_BEHIND_INTAKE = flipped(ChoreoVars.Poses.HUB_BEHIND_INTAKE);
+
+    // ============= ZONES =============
+    // Blue-alliance-relative corners of the zone where a shot should PASS
+    // instead of SCORE.
+    private static final Translation2d PASSING_ZONE_CORNER_A = new Translation2d(5.0, -2.0);
+    private static final Translation2d PASSING_ZONE_CORNER_B = new Translation2d(24.0, 10.0);
+
+    public static final Supplier<Rectangle2d> PASSING_ZONE = flipped(
+            new Rectangle2d(PASSING_ZONE_CORNER_A, PASSING_ZONE_CORNER_B));
 
     private POI() {
     }
