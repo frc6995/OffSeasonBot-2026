09f2975b02befd21b16c7107c73ae26dcee45669
Jeremy <maplecake199@gmail.com>
2026-09-25 15:36:20 -0700

    make a branch from “.” With accurate map and drive ratio

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -105,7 +105,7 @@ public class TunerConstants {
     // This may need to be tuned to your individual robot
     private static final double kCoupleRatio = 3.125;
 
-    private static final double kDriveGearRatio = 8.76615384615;
+    private static final double kDriveGearRatio = 7.671; //8.76615384615;
     private static final double kSteerGearRatio = 21.428571428571427;
     private static final Distance kWheelRadius = Inches.of(2);
 
